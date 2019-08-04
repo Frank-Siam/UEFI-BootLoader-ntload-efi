253c10e9b323bf74c7a9b8a9723303e50f0adb89
Frank Coder <36799737+Frank-Siam@users.noreply.github.com>
2019-08-04 14:59:48 +0700

    init
    init the file with some comments

---
+++ winload.c
@@ -0,0 +1,6 @@
+// winload.c
+// from this we build the winload32.efi or winload64.efi
+
+// winload will use UEFI services for low level functions
+
+// winloadXX.efi will be loaded from UEFI or from a bootmanager and cmd-line arguments can be passed to winloadXX.efi
