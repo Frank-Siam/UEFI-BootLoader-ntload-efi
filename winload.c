a21e6f30d7d30c40d37b96741232bf6d87990685
Frank-Siam <36799737+Frank-Siam@users.noreply.github.com>
2019-08-04 15:56:19 +0700

    Update winload.c
    test some file changes

--- winload.c
+++ winload.c
@@ -4,3 +4,15 @@
 // winload will use UEFI services for low level functions
 
 // winloadXX.efi will be loaded from UEFI or from a bootmanager and cmd-line arguments can be passed to winloadXX.efi
+
+#include "efi.h"
+
+EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable)
+{
+
+// if we just return and not called ExitBoot.. the system consider as loadFail and offer other bootmode or simply warmboot
+// if we did ExitBoot then it restart normal and consider as last known good
+// but a normal successful Load with ExitBoot will never return here, instead the kernel will  rt.ResetSystem (shutdown, warm-reboot, cold-reboot)
+
+return EFI_SUCCESS;
+}
